ccfaabf4cf0d19cb1c36964f82c7f756c8260137
Pratiksha Deshmukh <pratikshadeshmukh611@gmail.com>
2021-09-29 09:17:56 +0530

    Add public key retrival policy to mysql connection string

--- mdep_backend/src/main/java/com/pratiket/MdepBackendApplication.java
+++ mdep_backend/src/main/java/com/pratiket/MdepBackendApplication.java
@@ -25,53 +25,7 @@ import java.util.Map;
 @Service
 public class MdepBackendApplication
 {
-    @Autowired
-    private ConnectionRepository connectionRepository;
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MdepBackendApplication.class, args);
-//        String str = "[{\"from\":{\"connectionConfig\":[{\"connectionConfigId\":\"ff8081817baa2ed6017baa2f95db0002\",\"connectionName\":\"mysql_con1\",\"connectionConfiguration\":\"{\\\"host\\\": \\\"localhost\\\",\\\"port\\\": \\\"3306\\\", \\\"username\\\": \\\"root\\\",\\\"password\\\": \\\"Newuser@123\\\",\\\"database\\\":\\\"exam\\\", \\\"table\\\":\\\"EMP\\\"}\"}],\"connectionType\":\"MYSQL\",\"connectionId\":\"ff8081817b96aca7017b96acccd40000\"},\"to\":{\"connectionConfig\":[{\"connectionConfigId\":\"ff8081817baa2ed6017baa2f95db0002\",\"connectionName\":\"mysql_con1\",\"connectionConfiguration\":\"{\\\"host\\\": \\\"localhost\\\",\\\"port\\\": \\\"3306\\\", \\\"username\\\": \\\"root\\\",\\\"password\\\": \\\"Newuser@123\\\",\\\"database\\\":\\\"exam\\\", \\\"table\\\":\\\"EMP2\\\"}\"}],\"connectionType\":\"MYSQL\",\"connectionId\":\"ff8081817b96aca7017b96acccd40000\"}}]";
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        List<LinkedHashMap<String, ConnectionDTO>> jobExecutionList = objectMapper.readValue(str, List.class);
-//        System.out.println(jobExecutionList);
-//        System.out.println(jobExecutionList.get(0));
-//        System.out.println(jobExecutionList.get(0).get("from"));
-//        System.out.println(jobExecutionList.get(0).get("to"));
-
-
     }
-
-    private void test() throws Exception {
-        /*ObjectMapper objectMapper = new ObjectMapper();
-        String str = "{\"connectionConfig\":[{\"connectionName\":\"pratu_con\",\"connectionConfiguration\":\"{\\\"ip\\\": \\\"192.32.22.32\\\",\\\"port\\\": \\\"3306\\\"}\"}],\"connectionType\":\"MYSQL\"}";
-        ConnectionDTO connectionDTO = objectMapper.readValue(str, ConnectionDTO.class);
-        System.out.println(connectionDTO);
-        Connection connection1 = new Connection();
-        BeanUtils.copyProperties(connectionDTO, connection1);
-        Connection connection2 = connectionRepository.save(connection1);
-        System.out.println(connection2);*/
-    }
-    /*
-    MYSQL -
-    {
-  "connectionConfig": [
-    {
-      "connectionName": "mysql_con1",
-      "connectionConfiguration": "{\"host\": \"localhost\",\"port\": \"3306\", \"username\": \"root\",\"password\": \"Newuser@123\"}"
-    }
-  ],
-  "connectionType": "MYSQL"
-}
-
-MONGO -
-{
-  "connectionConfig": [
-    {
-      "connectionName": "mongo_con1",
-      "connectionConfiguration": "{\"host\": \"localhost\",\"port\": \"27017\", \"username\": \"root\",\"password\": \"root\"}"
-    }
-  ],
-  "connectionType": "MONGODB"
-}
-     */
 }
